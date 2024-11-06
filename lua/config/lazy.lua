05d41c3c43b016f7ac0e5821a4a9cb6aa57124f3
Tjitte de Vries <tjittedevries@tres.nl>
2024-11-06 10:06:08 +0100

    neogen

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,11 +20,11 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.coding.neogen" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.vue" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.util.octo" },
 
     -- import/override with your plugins
     { import = "plugins" },
